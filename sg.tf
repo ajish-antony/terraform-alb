ab3640ae2fab32e0791d728effc84621ecca0bd2
Ajish Antony <48723128+ajish-antony@users.noreply.github.com>
2021-09-02 23:45:55 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,53 @@
+resource "aws_security_group" "sg" {
+    
+  name_prefix       = "${var.project}-sg--"
+  description       = "allows 80 443 22"
+  vpc_id            = aws_vpc.main.id
+
+  ingress {
+    
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  ingress {
+    
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+   
+  egress {
+    
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+    
+
+  tags = {
+    Name = "${var.project}-sg"
+  }
+
+
+}
+
+output "Security_Group" {
+  value = aws_security_group.sg.id
+}
